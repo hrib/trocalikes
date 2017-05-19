289b26696d8a346069dcc2371497804c28c5b0ab
hrib <hugo.dose@gmail.com>
2017-05-19 11:17:49 +0100

    Update gerencia.php

--- gerencia.php
+++ gerencia.php
@@ -15,7 +15,7 @@ SELECT * FROM tl_cadastro ORDER BY id;
 CREATE TABLE tl_cliques (id SERIAL, tempo TIMESTAMP, dono_id VARCHAR(30), dono_page VARCHAR(80), dono_post VARCHAR(100), clicker_id VARCHAR(30), clicker_check VARCHAR(15));
 INSERT INTO tl_cliques (tempo , dono_id , dono_page , dono_post , clicker_id , clicker_check) VALUES (now(), 'dono id 123', 'dono page site', 'dono post postagem', 'click erid identidade', 'esperando');  
     </textarea></div>
-  <div><textarea name="sql" rows="5" cols="200"><?php echo $query; ?></textarea></div>
+  <div><textarea name="sql" rows="50" cols="2000"><?php echo $query; ?></textarea></div>
   <div><input type="submit" value="Input"></div>
 </form>
 
